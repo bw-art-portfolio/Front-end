a485de8c0cee41a818df42df192f0f551dece8f0
Michelle Scott <scottmm374@gmail.com>
2019-07-31 19:08:06 -0400

    Push so merge to master

--- artportfolio/src/App.js
+++ artportfolio/src/App.js
@@ -9,25 +9,26 @@ import './App.css';
 
 class App extends Component {
   constructor() {
-    super ();
+    super();
     this.state = {
-      artists: []
+      artists: [],
     };
   }
 
-    componentDidMount() {
-      axios
-          .get('https://artportfoliobw.herokuapp.com/')
-          .then(res => {
-            console.log(res.data);
-            
-              this.setState({ artists: res.data })
-          })
-          .catch(err => {
-              console.log(err)
-          })
-    }
-  
+  componentDidMount() {
+    axios
+      .get('https://artportfoliobw.herokuapp.com/')
+      .then(res => {
+
+        // console.log(res.data);
+
+        this.setState({ artists: res.data })
+      })
+      .catch(err => {
+        console.log(err)
+      })
+  }
+
 
   render() {
     console.log('app', this.state)
@@ -38,16 +39,21 @@ class App extends Component {
             <NavLink exact activeClassName='active' to="/">Home</NavLink>
             <NavLink exact activeClassName='active' to="/login">Login</NavLink>
             <NavLink exact activeClassName='active' to="/register">Register</NavLink>
-           
+            <NavLink exact activeClassName='active' to="/artist">Artist Page</NavLink>
 
-            </nav>
-              
-           
-          {/* <Route exact path='/' component={ArtistList} /> */}
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/register' component={Register} />
-          <ArtistList artists={this.state.artists} />
-          <Artist artists={this.state.artists} />
+          </nav>
+          <section>
+            {/* <Route exact path='/' component={ArtistList} /> */}
+            <Route path='/login' component={Login} />
+            <Route path='/register' component={Register} />
+            {/* <Route path='/artist' component={Artist} /> */}
+          </section>
+
+          <section>
+            <ArtistList artists={this.state.artists} />
+            <Artist artists={this.state.artists} />
+          </section>
+
         </header>
       </div>
     );
